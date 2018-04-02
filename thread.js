890645439bccc23a0b29a33250f93456962d5f8b
Felipe Romani <felipearomani@gmail.com>
2018-04-01 21:43:52 -0300

    Criado codigo para testar thread pool

---
+++ thread.js
@@ -0,0 +1,25 @@
+process.env.UV_THREADPOOL_SIZE = 5;
+
+const crypto = require('crypto');
+
+const startDate = Date.now();
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('1:', Date.now() - startDate);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('2:', Date.now() - startDate);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('3:', Date.now() - startDate);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('4:', Date.now() - startDate);
+});
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('5:', Date.now() - startDate);
+});
