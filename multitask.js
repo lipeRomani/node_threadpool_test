cf53323dd146e49be2cc9950a7448550a5734761
Felipe Romani <felipearomani@gmail.com>
2018-04-01 23:07:02 -0300

    Adicionado alguns comentários

--- multitask.js
+++ multitask.js
@@ -7,6 +7,7 @@ const crypto = require('crypto');
 /** @type Number */
 const start = Date.now()
 
+// Https ou HTTP não é processado pelo Threadpool, ele vai direto para o SO processar.
 function doRequest() {
     https
         .request("https://www.google.com", res => {
@@ -18,12 +19,14 @@ function doRequest() {
         .end();
 }
 
+// pacote crypto algumas funcoes usam o Threadpool como é o caso do pbkdf2
 function doHash() {
     crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
         console.log('hash:', Date.now() - start);
     });
 }
 
+// o modulo fs todas as funcoes usam o Threadpool do libUV
 function doDisk() {
     fs.readFile('multitask.js', 'utf8', () => {
         console.log('fs:', Date.now() - start);
